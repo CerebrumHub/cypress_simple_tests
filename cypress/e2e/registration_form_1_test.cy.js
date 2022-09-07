6b4deb7b144bb069017e691a67a1e23b5095399a
Kristofer Jermolov <kristofer.jermolov@gmail.com>
2022-09-07 13:39:43 +0300

    MEN-22 - empty username test, beforeeach added for all tests to load, checking :invalid errors

--- cypress/e2e/registration_form_1_test.cy.js
+++ cypress/e2e/registration_form_1_test.cy.js
@@ -1,20 +1,29 @@
+beforeEach(()=>{
+    cy.visit('cypress/fixtures/registration_form_1.html')
+})
+
 describe('This is first test suite', ()=>{
     it('User can submit data only when valid mandatory values are added', ()=> {
-        cy.visit('cypress/fixtures/registration_form_1.html')
         cy.get('#username').type('Tester 123')
         cy.get('.phoneNumberTestId').type('1020304050')
         cy.get('input[name="password"]').type('MyPass')
         cy.get('[name="confirm"]').type('MyPass')
         cy.get('.submit_button').click()
 
+        // Assert that all mandatory fields with class .input do not have error
+        cy.get('.input').should('not.have.class', ':invalid')
     });
 
     it('User cannot submit data when username is absent', ()=>{
-        // Open page
-        // Fill values
-            // Phone number
-            // Only valid passwords
-        // Assert that click button is not enabled
+        // Input backspace - will left the input as empty
+        cy.get('#username').type('{backspace}')
+        cy.get('#username').should('have.class', ':invalid')
+        cy.get('.phoneNumberTestId').type('1020304050')
+        cy.get('input[name="password"]').type('MyPass')
+        cy.get('[name="confirm"]').type('MyPass')
+
+        // Asserting that Submit button is disabled
+        cy.get('.submit_button').should('be.disabled')
     })
 
     it('User can use only numbers for phone number', ()=>{
@@ -34,4 +43,7 @@ describe('This is first test suite', ()=>{
         // Click submit button
         // Assert that no error is present
     })
+
+    // Ideas: https://stackoverflow.com/questions/65208169/how-to-validate-a-error-message-in-cypress it is possible to check
+    // error message
 })
