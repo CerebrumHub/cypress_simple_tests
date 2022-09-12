683c756cfb57e9748b6904046c6c5cb4766f20dd
Kristofer Jermolov <kristofer.jermolov@gmail.com>
2022-09-12 13:01:42 +0300

    MEN-22 All tests for registration form 1 corrected, repetitive actions use functions

--- cypress/e2e/registration_form_1_test.cy.js
+++ cypress/e2e/registration_form_1_test.cy.js
@@ -14,7 +14,7 @@ beforeEach(() => {
 // 5. Add comment to the first test containing today’s date
 
 describe('This is first test suite', () => {
-    it.only('User can submit data only when valid mandatory values are added', () => {
+    it('User can submit data only when valid mandatory values are added', () => {
         cy.get('#username').type('Something')
         cy.get('#firstName').type('John')
         cy.get('#lastName').type('Doe')
@@ -22,7 +22,7 @@ describe('This is first test suite', () => {
         cy.get('input[name="password"]').type('MyPass')
         cy.get('[name="confirm"]').type('MyPass')
         cy.get('h2').contains('Password').click()
-        cy.get('.submit_button', { timeout: 10000 }).should('be.enabled');
+        cy.get('.submit_button', {timeout: 10000}).should('be.enabled');
         cy.get('.submit_button').click()
 
         // Assert that both input and password error messages are not shown
@@ -32,23 +32,19 @@ describe('This is first test suite', () => {
         cy.get('#success_message').should('have.css', 'display', 'block')
     });
 
-    it.only('User cannot submit data when username is absent', () => {
-        // Checking form validation without making actions on form
-        cy.get('#applicationForm').then(({$form}) => {
-            expect($form[0].checkValidity()).to.be.false
-        })
-        cy.get('#form-validation :invalid').should('have.length', 1)
-
-        cy.get('#username').clear()
-        cy.get('#username').should('have.class', ':invalid')
-        cy.get('input[name="username"]:invalid').invoke('prop', 'validationMessage').should('equal', 'Please fill out this field.')
-        cy.get('#phoneNumberTestId').type('1020304050')
-        cy.get('input[name="password"]').type('MyPass')
-        cy.get('[name="confirm"]').type('MyPass')
+    it('User cannot submit data when username is absent', () => {
+        applicationFormWithInvalidUsername('{backspace}', 'Please fill out this field.')
 
         // Asserting that Submit button is disabled
-        // Assert that input error message is visible
         // Assert that success message is not visible
+        cy.get('.submit_button').should('be.disabled')
+        cy.get('#success_message').should('have.css', 'display', 'none')
+        //NB! In that case error message will not appear as with invalid data test case, this is known issue
+    })
+
+    it('User cannot submit invalid characters as username', () => {
+        applicationFormWithInvalidUsername(' ', 'Please match the requested format.')
+
         cy.get('.submit_button').should('be.disabled')
         cy.get('#input_error_message').should('have.css', 'display', 'block')
         cy.get('#success_message').should('have.css', 'display', 'none')
@@ -56,18 +52,36 @@ describe('This is first test suite', () => {
 
     it('User can use only numbers for phone number', () => {
         // Asserting that attribute type=number is present - only numbers are supported
-        cy.get('#phoneNumberTestId').should('have.attr', 'type', 'number')
+        cy.get('[data-testid="phoneNumberTestId"]').should('have.attr', 'type', 'number')
     })
 
     it('User can use only same both first and validation passwords', () => {
-        cy.get('#username').clear()
-        cy.get('#username').should('have.class', ':invalid')
-        cy.get('#phoneNumberTestId').type('1020304050')
-        cy.get('input[name="password"]').type('MyPass')
-        cy.get('[name="confirm"]').type('Another')
+        applicationFormWithInvalidUsername('{backspace}', 'Please fill out this field.')
+        cy.get('[data-testid="phoneNumberTestId"]').type('1020304050')
+        inputPasswords('MyPassword', 'Another')
         cy.get('#username').type('{enter}')
 
         cy.get('#password_error_message').should('have.css', 'display', 'block')
         cy.get('#success_message').should('have.css', 'display', 'none')
     })
+
+    function applicationFormWithInvalidUsername(username, tooltip) {
+        // Checking form validation without making actions on form
+        cy.get('#applicationForm').then(
+            ($form) => expect($form[0].checkValidity()).to.be.false,
+        )
+        cy.get('#applicationForm :invalid').should('have.length', 1)
+
+        // Checking tooltip with invalid data
+        cy.get('#username').clear().type(`${username}`)
+        cy.get('h2').contains('Password').click()
+        cy.get('input[name="username"]:invalid').invoke('prop', 'validationMessage').should('equal', `${tooltip}`)
+        cy.get('[data-testid="phoneNumberTestId"]').type('10203040')
+        inputPasswords('MyPassword', 'MyPassword')
+    }
+
+    function inputPasswords(input, confirmation) {
+        cy.get('input[name="password"]').type(`${input}`)
+        cy.get('[name="confirm"]').type(`${confirmation}`)
+    }
 })
