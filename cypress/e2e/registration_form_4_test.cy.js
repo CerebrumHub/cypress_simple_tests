a32518c54ee064a4b1604c0669c95a3a1e2c1b93
Kristofer Jermolov <kristofer.jermolov@gmail.com>
2022-10-01 00:43:16 +0300

    Registration form 4

--- cypress/e2e/registration_form_4_test.cy.js
+++ cypress/e2e/registration_form_4_test.cy.js
@@ -3,31 +3,27 @@ beforeEach(() => {
 })
 
 // Workshop #10 analyze and fix failed test
-
-describe('Section 1: Functional tests', () => {
-    it('Check that logo is correct and has correct size', () => {
-       //TODO
-    })
-})
-
 describe('Input fields', ()=>{
     it('Username cannot be empty string', ()=>{
-        cy.get('#username').type('{backspace}')
+        cy.get('#username').type(' ')
         cy.window().scrollTo('bottom')
+        cy.get('h2').contains('Password').click()
         cy.get('#input_error_message').should('be.visible')
         cy.get('#success_message').should('not.be.visible')
         cy.get('#password_error_message').should('have.css', 'display', 'none')
-        cy.get('.submit_button').click()
     })
 
     it('Username tooltip changes depending on input value', ()=>{
-        // use applicationFormWithInvalidUsername
-
         // get username
         // empty field should show - Please add username
         // add faulty string
         // input field with wrong format - Input field contains not supported character
         // If data is valid then no tooltip is present
+        cy.get('#username').type('{backspace}')
+        cy.get('h2').contains('Password').click()
+        cy.get('#username').should('have.attr', 'title').should('contain', 'Please add username')
+        cy.get('#username').should('have.css', 'box-shadow').should('contain', 'rgb(255, 0, 0)')
+        cy.get('input[name="username"]:invalid').invoke('prop', 'validationMessage').should('contain', 'fill out this field')
     })
 
     it('Username support only string characters', ()=>{
@@ -36,14 +32,23 @@ describe('Input fields', ()=>{
         // check that error at the bottom is correct
         // Check that tooltip is correct
         // submit button is not active
+        cy.get('input[name="username"]:invalid')
+            .invoke('prop', 'validationMessage').should('contain', 'fill out this field')
+        cy.get('input[name="username"]').type('=')
+        cy.get('h2').contains('Password').click()
+        cy.get('input[name="username"]').should('have.css', 'box-shadow').should('contain', 'rgb(255, 0, 0)')
+        cy.get('#input_error_message').should('be.visible').should('have.css', 'display', 'block')
+        cy.get('.submit_button').should('not.be.enabled');
     })
 
     it('Username should have max length of 50 characters', ()=>{
         // check that HTML has max attribute value
+        cy.get('#username').should('have.attr', 'max', '50')
     })
 
     it('Username should support only lower letters and numbers', ()=>{
         // check with regex supporter format
+        cy.get('#username').should('have.attr', 'pattern', '[a-zA-Z0-9_]+')
     })
 
     it('Email input should support correct pattern', ()=>{
@@ -55,7 +60,13 @@ describe('Input fields', ()=>{
         // field should have correct CSS style
         // submit button should not be active
         // change to valid email
-        // submit button should be selectable
+        cy.get('#email').should('have.attr', 'pattern').should('contain', '@')
+        cy.get('#email:invalid').invoke('prop', 'validationMessage').should('contain', 'fill out this field')
+        cy.get('#email').type('invalid')
+        cy.get('h2').contains('Password').click()
+        cy.get('#email').should('have.css', 'color').should('contain', 'rgb(0, 0, 0)')
+        cy.get('.submit_button').should('not.be.enabled');
+        cy.get('#email').clear().type('invalid@email.com')
     })
 
     it('Passwords cannot be empty string', ()=>{
@@ -63,27 +74,32 @@ describe('Input fields', ()=>{
         // input empty password
         // input confirm password also as empty
         // Check that submit button is not active
-    })
-
-    it('Table should contain correct data', ()=>{
-        // Check that table has correct size
-        // Check data inside table
-        // Check that Faulty value is not present in table
+        cy.get('.input.confirm').type('{backspace}')
+        cy.get('[name="confirm"]').type('{backspace}')
+        cy.get('h2').contains('Password').click()
+        cy.get('.submit_button').should('not.be.enabled');
     })
 
     it('User cannot submit empty registration form', ()=>{
         // Empty all input fields
         // Check that submit button is not present
+        cy.get('input[type=text]').clear()
+        cy.get('.submit_button').should('not.be.enabled');
     })
 
     it('HTML should be present in Web Languages radio buttons list', ()=>{
         // get list
         // check that at least one of elements is HTML
+        cy.get('input[type=radio]').next().then(labelsOfRadioButtons => {
+            console.log('Here will be radio buttons:' + `${labelsOfRadioButtons}`)
+            const actual = [...labelsOfRadioButtons].map(singleRadioButtonLabel => singleRadioButtonLabel.innerText)
+            expect(actual).to.deep.eq(['HTML', 'CSS', 'JavaScript', 'PHP'])
+        })
     })
 
     it('BMW should not be listed in cars list', ()=>{
         // Check list does not contain BMW
+        cy.get('#cars').children().should('have.length', 4)
+        cy.get('#cars').first().should('not.have.text', 'Bayerische Motoren Werke')
     })
-
-
 })
