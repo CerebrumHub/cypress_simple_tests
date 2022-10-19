cc16bb0be04a309b9200efe007212c9eb3af6f99
Kristofer Jermolov <kristofer.jermolov@gmail.com>
2022-10-19 12:54:06 +0300

    Deleted allure

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,14 @@
 const {defineConfig} = require("cypress");
-const AllureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
     pageLoadTimeout: 8000,
 
     env: {
         firstCookieValue: "firstValue",
-        allure: true,
-        allureAttachRequests: true,
-        allureAddVideoOnPass: true
     },
 
     e2e: {
         setupNodeEvents(on, config) {
-            // AllureWriter(on, config);
             return config;
         }
     },
